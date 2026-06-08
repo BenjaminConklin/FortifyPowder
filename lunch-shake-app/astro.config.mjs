f7ae09442fe05524e6a1fa74f29febf6922656f5
Benjamin Conklin <benjaminandco@icloud.com>
2026-06-07 19:43:14 -0600

    corrected the url and repo names

--- lunch-shake-app/astro.config.mjs
+++ lunch-shake-app/astro.config.mjs
@@ -7,6 +7,6 @@ export default defineConfig({
       vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://benjaminconklin.github.io',
-  base: '/LunchShakeCalc',
+  site: 'https://BenjaminConklin.github.io',
+  base: '/FortifyPowder',
 });
